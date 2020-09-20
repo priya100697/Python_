55c62b9b2999022b4407d0ac9ccea56a4535d3dd
Priya <39254688+priya100697@users.noreply.github.com>
2020-09-20 23:51:11 +0530

    Update Main.py

--- Database/Main.py
+++ Database/Main.py
@@ -5,14 +5,14 @@ from Database.DbConnect import get_connection
 # Ref: https://www.psycopg.org/docs/usage.html#passing-parameters-to-sql-queries
 if __name__ == "__main__":
     conn = get_connection()
-    #conn = psycopg2.connect("dbname=Pdb user=postgres password=toor")
+    #conn = psycopg2.connect("dbname=Pdb user=postgres password=root")
     cur = conn.cursor()
 
     cur.execute("select * from employee")
     data = cur.fetchall()
     print("Number of records before insertion = ", data)
 
-    cur.execute("insert into employee(id, name) values(%s, %s)", (1, "anand"))
+    cur.execute("insert into employee(id, name) values(%s, %s)", (1, "priya"))
 
     cur.execute("select count(*) from employee")
     data = cur.fetchall()
